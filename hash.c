ac97750f4eeea0a6bb65a68a8b9a841cb23bd8f8
samara <samara@Samaras-MacBook-Pro.local>
2018-10-11 19:53:08 -0300

    bla

--- hash.c
+++ hash.c
@@ -1,28 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
+//#include <math.h>
 
-long double convert(char cpf[12]){
-	long double key;
-	int i;
-	char c;
-	for(i=0;i<9;i++){
-		c=cpf[i];
-		key=key+(c-'0')*powl(10,10-i);
-	}
-	for(i=10;i<12;i++){
-		c=cpf[i];
-		key=key+(c-'0')*powl(10,11-i);
-	}
+long convert(char cpf[13]){
+	long key;
+	char cpf2[12];
+	cpf2[11]='\0';
+	strncpy(cpf2,cpf,9);
+	strcat(cpf2,&cpf[10]);
+	printf("%s\n",cpf2);
+	key = strtol(cpf2, NULL, 10);
 	return key;
 }
 
 //int hash(int cpf)
 
 int main(){
-	char cpf[12] = "123456789-00";
-	long double key;
+	char cpf[13] = "123456789-12";
+	cpf[12]='\0';
+	long key;
 	key=convert(cpf);
-	printf("%Lf",key);
+	printf("%ld",key);
 	return 0;
 }
