e489ab7eccf83dac98adca97fac53cb9c122680b
samara <samara@Samaras-MacBook-Pro.local>
2018-10-11 18:49:48 -0300

    bla

--- hash.c
+++ hash.c
@@ -1,4 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-int hash(char cpf[12] )
+long convert(char cpf[12]){
+	long cpf;
+	cpf = strtol(cpf, cpf[8], 10);
+	strcat(cpf,strtol(cpf[10],cpf[11],10));
+}
+
+//int hash(int cpf)
+
+int main(){
+	return 0;
+}
