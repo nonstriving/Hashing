fa149d31df63d62e36b7888c23275b86cde2eb05
samara <samara@Samaras-MacBook-Pro.local>
2018-10-11 19:01:23 -0300

    bla

--- hash.c
+++ hash.c
@@ -3,9 +3,15 @@
 #include <math.h>
 
 long convert(char cpf[12]){
-	long cpf;
-	cpf = strtol(cpf, cpf[8], 10);
-	strcat(cpf,strtol(cpf[10],cpf[11],10));
+	int n;
+	int i;
+	for(i=0;i<9;i++){
+		n=n+(c-'0')*(10**(10-i));
+	}
+	for(i=10;i<12;i++){
+		n=n+(c-'0')*(10**(11-i))
+	}
+	return n;
 }
 
 //int hash(int cpf)
